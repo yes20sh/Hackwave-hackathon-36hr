8b137db8f8521780f016070f23c7c0b34db84400
tushar19chouhan <tushar19chouhan@gmail.com>
2025-08-24 02:03:46 +0530

    search done

--- frontend/src/components/search-page/SearchPage.jsx
+++ frontend/src/components/search-page/SearchPage.jsx
@@ -1,32 +1,39 @@
-import React from 'react'
-import Container from '../container/Container'
-import animationBackground from '../../assets/animation-background.mp4'
+import React from "react";
+import Container from "../container/Container";
+import animationBackground from "../../assets/animation-background.mp4";
+import SearchBox from "../searchbox/SearchBox";
+import Navbar from "../navbar/Navbar";
 
 const Search = () => {
   return (
     <Container>
-      <div className="relative w-screen h-screen overflow-hidden">
-        {/* Background Video */}
+      <div className="relative w-full h-screen overflow-hidden">
+        {/* Navbar fixed at top */}
+        <div className="absolute top-0 left-0 w-full z-20">
+          <Navbar />
+        </div>
+
+        {/* Background Video (Responsive + Centered) */}
         <div className="absolute inset-0 flex items-center justify-center">
           <video
             autoPlay
             loop
             muted
             playsInline
-            className="w-[700px] h-[700px] object-cover"
+            className="object-cover w-[90vw] max-w-[700px] aspect-square"
           >
             <source src={animationBackground} type="video/mp4" />
             Your browser does not support the video tag.
           </video>
         </div>
 
-        {/* Overlay Content */}
-        <div className="relative z-10 flex items-center justify-center h-full">
-          <h1 className="text-white text-4xl font-bold">Hello World</h1>
+        {/* Overlay Content (below Navbar, centered) */}
+        <div className="relative z-10 flex items-center justify-center h-full pt-20 px-4">
+          <SearchBox />
         </div>
       </div>
     </Container>
-  )
-}
+  );
+};
 
-export default Search
+export default Search;
